fd2d7eb97f1d4c8e1b1426b2a7476e851876b32c
Bahadur001 <161586965+Bahadur001@users.noreply.github.com>
2024-05-19 23:53:08 +0400

    commit

---
+++ main10.py
@@ -0,0 +1,19 @@
+# task1
+# import os
+# import shutil
+
+# example_dir = "Example"
+# os.makedirs(example_dir, exist_ok=True)
+
+# sub_dir = os.path.join(example_dir, "subdirect")
+# os.makedirs(sub_dir, exist_ok=True)
+
+# task2
+# def yerleri_goster(xallar):
+#     siralanmis_xallar = sorted(zip(xallar, ['1-ci', '2-ci', '3-cü', '4-cü', '5-ci']))
+
+#     for xal, yer in siralanmis_xallar:
+#         print(f"{yer} - {xal} xal")
+
+# xallar = [5, 3, 4, 2, 1]
+# yerleri_goster(xallar)
